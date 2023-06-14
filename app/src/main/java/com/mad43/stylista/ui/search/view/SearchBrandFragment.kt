c79ce3d6cfa42a9b0f84c08588f40cf3e7e35616
Marina <marina.abdelmalak2019@gmail.com>
2023-06-14 12:47:55 +0300

    search for brand in home and search for product

--- app/src/main/java/com/mad43/stylista/ui/search/view/SearchBrandFragment.kt
+++ app/src/main/java/com/mad43/stylista/ui/search/view/SearchBrandFragment.kt
@@ -1,40 +1,23 @@
 package com.mad43.stylista.ui.search.view
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.mad43.stylista.R
-import com.mad43.stylista.databinding.FragmentLogInBinding
 import com.mad43.stylista.databinding.FragmentSearchBinding
-import com.mad43.stylista.domain.model.DisplayBrand
-import com.mad43.stylista.ui.brand.BrandAdapter
 import com.mad43.stylista.ui.home.HomeBrandAdapter
-import com.mad43.stylista.ui.home.HomeFragmentDirections
-import com.mad43.stylista.ui.home.HomeViewModel
 import com.mad43.stylista.ui.home.OnItemBrandClicked
 import com.mad43.stylista.ui.search.viewModel.SearchViewModel
-import com.mad43.stylista.util.Constants
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 
-class SearchFragment : Fragment() , OnItemBrandClicked {
+class SearchBrandFragment : Fragment() , OnItemBrandClicked {
 
     private lateinit var searchViewModel: SearchViewModel
     private lateinit var brandAdapter: HomeBrandAdapter
@@ -64,7 +47,7 @@ class SearchFragment : Fragment() , OnItemBrandClicked {
         val toolbar = activity.findViewById<androidx.appcompat.widget.Toolbar>(R.id.materialToolbar)
         activity.setSupportActionBar(toolbar)
 
-        brandAdapter = HomeBrandAdapter(requireContext(), this@SearchFragment)
+        brandAdapter = HomeBrandAdapter(requireContext(), this@SearchBrandFragment)
         binding.searchByNameRecyclerView.adapter = brandAdapter
 
         binding.searchByNameRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
