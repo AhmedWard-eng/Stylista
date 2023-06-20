f6a50b82e757b627d71f8604abf93de83033217d
Marina <marina.abdelmalak2019@gmail.com>
2023-06-21 01:57:09 +0300

    handle

--- app/src/main/java/com/mad43/stylista/ui/search/view/SearchProductFragment.kt
+++ app/src/main/java/com/mad43/stylista/ui/search/view/SearchProductFragment.kt
@@ -59,13 +59,15 @@ class SearchProductFragment : Fragment() , OnItemProductClicked {
 
         var searchView = toolbar.findViewById<android.widget.EditText>(R.id.searchView)
         searchView.hint = getString(R.string.search_product)
+        searchViewModel.getAllProduct()
+        brandAdapter.submitList(searchViewModel.allPrpduct)
         searchView.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 searchViewModel.getAllProduct()
                     brandAdapter.submitList(searchViewModel.allPrpduct)
             }
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val searchTextContent = searchView.text.toString()
+                val searchTextContent = s.toString()
                 val filteredProducts = searchViewModel.searchAllProduct(searchTextContent)
                 brandAdapter.submitList(filteredProducts)
             }
