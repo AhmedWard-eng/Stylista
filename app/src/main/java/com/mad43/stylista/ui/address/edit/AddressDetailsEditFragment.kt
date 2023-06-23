13ddf54de59180304bf15608e723770e3d05e21e
ahmed ward <ahmedward221@gmail.com>
2023-06-23 22:33:57 +0300

    address

--- app/src/main/java/com/mad43/stylista/ui/address/edit/AddressDetailsEditFragment.kt
+++ app/src/main/java/com/mad43/stylista/ui/address/edit/AddressDetailsEditFragment.kt
@@ -1,5 +1,9 @@
 package com.mad43.stylista.ui.address.edit
 
+import android.content.Context
+import android.location.Address
+import android.location.Geocoder
+import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
@@ -13,12 +17,19 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.Navigation
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import com.mad43.stylista.R
 import com.mad43.stylista.databinding.FragmentAddressesBinding
 import com.mad43.stylista.domain.model.AddressItem
+import com.mad43.stylista.util.Constants
 import com.mad43.stylista.util.MyDialog
 import com.mad43.stylista.util.RemoteStatus
 import kotlinx.coroutines.launch
+import java.util.Locale
 
 class AddressDetailsEditFragment : Fragment() {
 
@@ -36,6 +47,7 @@ class AddressDetailsEditFragment : Fragment() {
     ): View {
         viewModel = ViewModelProvider(this)[AddressDetailsEditViewModel::class.java]
         _binding = FragmentAddressesBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
@@ -45,13 +57,43 @@ class AddressDetailsEditFragment : Fragment() {
         if (address.addressId != -1L) {
             viewModel.isToEdit = true
             binding.checkBox.visibility = GONE
-            binding.address1.setText(address.address1, TextView.BufferType.EDITABLE)
+            binding.addressTitle.setText(address.address1, TextView.BufferType.EDITABLE)
             binding.address2.setText(address.address2, TextView.BufferType.EDITABLE)
             binding.addressCity.setText(address.city, TextView.BufferType.EDITABLE)
             binding.addressPhone.setText(address.phone, TextView.BufferType.EDITABLE)
             binding.editTextProvince.setText(address.province, TextView.BufferType.EDITABLE)
             binding.editTextCountry.setText(address.country, TextView.BufferType.EDITABLE)
         }
+
+        Places.initialize(requireContext().applicationContext,Constants.GOOGLE_KEY)
+        val autocompleteFragment = childFragmentManager.findFragmentById(R.id.autocomplete_fragment)
+                    as AutocompleteSupportFragment
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID,
+            Place.Field.NAME,
+            Place.Field.ADDRESS,
+            Place.Field.LAT_LNG))
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                place.address
+                // TODO: Get info about the selected place.
+                Log.i("TAG", "Place: ${place.address}")
+                val latLng = place.latLng
+                if( latLng != null){
+                    getAddress(requireContext(), latLng,::fillEditText,::showError)
+                }else{
+                    Log.d("TAG", "onPlaceSelected: $latLng")
+                }
+            }
+
+            override fun onError(status: Status) {
+                // TODO: Handle the error.
+                Log.i("TAG", "An error occurred: $status")
+            }
+        })
 
         binding.button.setOnClickListener {
             if (isAllDataCompleted()) {
@@ -91,8 +133,37 @@ class AddressDetailsEditFragment : Fragment() {
 
     }
 
+
+
+    private fun showError(e: Exception) {
+
+    }
+
+    private fun fillEditText(address: Address?) {
+
+        val provinceList = address?.adminArea?.trim()?.split("Gov")
+
+        Log.d("TAG", "fillEditText: $provinceList")
+        if (!provinceList.isNullOrEmpty()){
+            var governance = getGovernance(provinceList[0])
+            binding.addressCity.setText(address.subAdminArea, TextView.BufferType.EDITABLE)
+//        binding.addressPhone.setText(address.phone, TextView.BufferType.EDITABLE)
+            binding.editTextProvince.setText(governance, TextView.BufferType.EDITABLE)
+            binding.editTextCountry.setText(address.countryName, TextView.BufferType.EDITABLE)
+        }
+
+//        binding.addressTitle.setText(address., TextView.BufferType.EDITABLE)
+//        binding.address2.setText(address.address2, TextView.BufferType.EDITABLE)
+
+    }
+
+    private fun getGovernance(governance: String): String {
+        val go = viewModel.map[governance]
+        return go ?: governance
+    }
+
     private fun isAllDataCompleted(): Boolean {
-        return !(binding.address1.text.isNullOrBlank() ||
+        return !(binding.addressTitle.text.isNullOrBlank() ||
                 binding.address2.text.isNullOrBlank() ||
                 binding.addressCity.text.isNullOrBlank() ||
                 binding.addressPhone.text.isNullOrBlank() ||
@@ -104,7 +175,7 @@ class AddressDetailsEditFragment : Fragment() {
         return AddressItem(
             customerId = customerId,
             addressId = address.addressId,
-            address1 = binding.address1.text.toString(),
+            address1 = binding.addressTitle.text.toString(),
             address2 = binding.address2.text.toString(),
             city = binding.addressCity.text.toString(),
             phone = binding.addressPhone.text.toString(),
@@ -114,4 +185,30 @@ class AddressDetailsEditFragment : Fragment() {
 
         )
     }
+}
+
+fun getAddress(
+    context: Context, latLng: com.google.android.gms.maps.model.LatLng, onResult: (Address?) -> Unit,
+    onError:(Exception)-> Unit) {
+
+    try {
+        var address: Address?
+        val geocoder = Geocoder(context,Locale.US)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            geocoder.getFromLocation(
+                    latLng.latitude, latLng.longitude, 1
+                ) {
+                    address = it[0]
+                    onResult(address)
+                }
+        } else {
+            address = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)?.get(0)
+            onResult(address)
+        }
+    }catch (e: Exception){
+        onError(e)
+    }
+
 }
--- app/src/main/java/com/mad43/stylista/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mad43/stylista/ui/home/HomeViewModel.kt
@@ -8,16 +8,22 @@ import com.mad43.stylista.data.repo.product.ProductsRepo
 import com.mad43.stylista.domain.model.DisplayBrand
 import com.mad43.stylista.util.RemoteStatus
 import com.mad43.stylista.data.repo.product.ProductsRepoInterface
+import com.mad43.stylista.domain.model.CouponItem
+import com.mad43.stylista.domain.remote.GetCouponsListUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
-class HomeViewModel(private val repoInterface: ProductsRepoInterface = ProductsRepo()) :
+class HomeViewModel(private val repoInterface: ProductsRepoInterface = ProductsRepo(),private val getCouponsListUseCase: GetCouponsListUseCase = GetCouponsListUseCase()) :
     ViewModel() {
     val ads = ArrayList<SlideModel>()
     var brands = MutableStateFlow<RemoteStatus<List<DisplayBrand>>>(RemoteStatus.Loading)
+    private val _couponState = MutableStateFlow<RemoteStatus<CouponItem>>(RemoteStatus.Loading)
+    val couponState = _couponState.asStateFlow()
 
+    var couponCode = ""
     init {
         ads.add(SlideModel("https://picsum.photos/seed/picsum/200/300"))
         ads.add(SlideModel("https://picsum.photos/200/300"))
@@ -39,6 +45,25 @@ class HomeViewModel(private val repoInterface: ProductsRepoInterface = ProductsR
                 }
             } catch (e: Exception) {
                 brands.value = RemoteStatus.Failure(e)
+            }
+
+        }
+
+    }
+
+    fun getRandomCoupon(){
+        viewModelScope.launch {
+            when(val status = getCouponsListUseCase()){
+                is RemoteStatus.Success ->{
+                    _couponState.emit(RemoteStatus.Success(status.data.random()))
+                }
+
+                is RemoteStatus.Failure ->{
+                    _couponState.emit(status)
+                }
+                else ->{
+
+                }
             }
 
         }
